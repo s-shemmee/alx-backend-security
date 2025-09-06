4c4eb97f47ba64ec137f34c1700812110201db3f
s-shemmee <56132945+s-shemmee@users.noreply.github.com>
2025-09-06 13:58:03 +0100

    Update ALLOWED_HOSTS

--- settings.py
+++ settings.py
@@ -5,7 +5,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'replace-this-with-a-secure-key'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+shemmee.pythonanywhere.com]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
